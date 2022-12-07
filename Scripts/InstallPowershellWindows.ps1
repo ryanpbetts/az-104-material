ae75824b1e34c1de4ed02f089b2b1a84f758368f
Ryan Betts <ryan.betts@microsoft.com>
2022-12-07 20:48:43 +0000

    repo updates 07/12

---
+++ Scripts/InstallPowershellWindows.ps1
@@ -0,0 +1,13 @@
+﻿winget install --id Microsoft.Powershell --source winget 
+
+$PSVersionTable.PSVersion
+
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+
+Install-Module -Name Az -AllowClobber -Scope CurrentUser
+
+Connect-AzAccount
+
+Get-AzSubscription
+
+Set-AzContext -Subscription "sub id"
